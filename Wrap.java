f6ad7ea0620b88c3f1af7c0fc9fb804e5e0fb4ea
Suraj Kumar <rksurajrk351@gmail.com>
2026-07-02 20:11:34 +0530

    Add class A and enhance main method functionality
    Added class A with string fields and updated main method to parse and print additional values.

--- Wrap.java
+++ Wrap.java
@@ -1,3 +1,10 @@
+class A{
+String x="1456";
+String y="10101";
+
+}
+
+
 class Wrap{
 public static void main(String[]args){
 	int x=Integer.parseInt("123");
@@ -5,8 +12,14 @@ public static void main(String[]args){
 	int y=x1.intValue();
 	System.out.println(x);
 	System.out.println(y);
+	
+	A a1=new A();
+	double z=Integer.parseInt(a1.y,2);
+	double w=Double.parseDouble(a1.x);
+	
+	System.out.println(z);
+	System.out.println(w);
 
 	}
-
 
 }
