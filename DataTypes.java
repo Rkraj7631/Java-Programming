dae0f913b27da75fa54cc4a5ab96d736b0133cbb
Suraj Kumar <rksurajrk351@gmail.com>
2026-07-04 17:58:10 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,26 @@
+class DataTypes{
+public static void main(String[]args){
+	byte age=25;
+	short num=1000;
+	int salary =50000;
+	long population=2000000000L;
+	float pi=3.14f;
+	double price =25.02;
+	char grade ='A';
+	String name = "Suraj";
+	boolean pass =true;
+	boolean fail=false;
+	
+
+	System.out.println(age);
+	System.out.println(num);
+	System.out.println(salary);
+	System.out.println(population);
+	System.out.println(pi);
+	System.out.println(price);
+	System.out.println(grade);
+	System.out.println(name);
+	System.out.println(pass);
+	System.out.println(fail);
+	}
+}
